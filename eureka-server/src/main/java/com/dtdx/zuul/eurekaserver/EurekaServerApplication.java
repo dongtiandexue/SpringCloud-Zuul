7649c7692ea343a8a14e02d0ac8744d345babb79
dongtiandexue <huawei_code@163.com>
2019-03-02 16:39:28 +0800

    Eureka 服务端

---
+++ eureka-server/src/main/java/com/dtdx/zuul/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.dtdx.zuul.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * eureka服务端
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
